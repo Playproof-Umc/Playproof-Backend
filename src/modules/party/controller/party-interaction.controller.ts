44d9484575a86bf6228f2a537ca8f2c53ee494eb
si-zero <snm306@naver.com>
2026-01-09 14:17:47 +0900

    feat: 파티 컨트롤러 계층 추가

--- src/modules/party/controller/party-interaction.controller.ts
+++ src/modules/party/controller/party-interaction.controller.ts
@@ -1,29 +1,46 @@
-import { Controller, Post, Route, Tags, SuccessResponse, Path, Security, Request } from "tsoa";
+import { Controller, Patch, Post, Delete, Body, Path, Route, Tags, Security, Request, Response } from "tsoa";
 import { injectable, inject } from "tsyringe";
 import { PartyInteractionService } from "../service/party-interaction.service";
-import { Result } from "../../../common/types/result.type";
+import { PartyApplicationHandleReqDto } from "../dtos/party.req.dto";
+import { Result, ForbiddenError, NotFoundError, ConflictError } from "../../../common/types/result.type";
 
 @Route("parties")
-@Tags("Party")
+@Tags("Party Interaction")
 @injectable()
 export class PartyInteractionController extends Controller {
-  constructor(
-    @inject(PartyInteractionService) private interactionService: PartyInteractionService
-  ) {
+  constructor(@inject(PartyInteractionService) private interactionService: PartyInteractionService) {
     super();
   }
 
-  @SuccessResponse("200", "OK")
+  /** 가입 신청하기 */
   @Security("jwt")
-  @Post("{id}/like")
-  public async likeParty(
-    @Path() id: number,
+  @Post("/{partyId}/applications")
+  public async applyParty(@Path() partyId: number, @Request() req: any): Promise<Result<any>> {
+    const result = await this.interactionService.createApplication(req.user.id, partyId);
+    this.setStatus(result.statusCode);
+    return result;
+  }
+
+  /** 가입 신청 취소하기 */
+  @Security("jwt")
+  @Delete("/{partyId}/applications/{applicationId}")
+  public async cancelApply(@Path() partyId: number, @Path() applicationId: number, @Request() req: any): Promise<Result<any>> {
+    const result = await this.interactionService.cancelApplication(req.user.id, applicationId);
+    this.setStatus(result.statusCode);
+    return result;
+  }
+
+  /** 가입 신청 승낙/거절 (방장) */
+  @Security("jwt")
+  @Patch("/{partyId}/applications/{applicationId}")
+  public async handleApply(
+    @Path() partyId: number,
+    @Path() applicationId: number,
+    @Body() body: PartyApplicationHandleReqDto,
     @Request() req: any
-  ): Promise<Result<void>> {
-    const userId = req.user.id;
-    const result = await this.interactionService.likeParty(id, userId);
+  ): Promise<Result<any>> {
+    const result = await this.interactionService.handleApplication(req.user.id, applicationId, body.isAccepted);
     this.setStatus(result.statusCode);
     return result;
   }
 }
-
