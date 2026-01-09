e3bcc8adfb23a6b1fa5b216b1ca5d471f60d5a1e
rbdus0715 <rbdus0715@gmail.com>
2026-01-09 15:18:22 +0900

    feat: 파티 생성 dto 작성

--- src/modules/party/dtos/party.req.dto.ts
+++ src/modules/party/dtos/party.req.dto.ts
@@ -1,1 +1,49 @@
-export class PartyCreateReqDto {}
+import { IsBoolean, IsNumber, IsObject, IsString } from "class-validator/types";
+
+export class PartyCreateReqDto {
+    /*
+    * @example 1
+    */
+   @IsNumber()
+    gameId!: number;
+    /**
+     * @example "파티 제목"
+     */
+    @IsString()
+    title!: string;
+    /**
+     * @example "파티 내용"
+     */
+    @IsString() 
+    memo!: string;
+    /**
+     * @example 1
+     */  
+    @IsNumber()
+    recruitmentPeople!: number;
+    /**
+     * @example 1
+     */
+    @IsNumber()
+    tierId!: number;
+    /**
+     * @example 1
+     */
+    @IsNumber()
+    positionId!: number;
+    /**
+     * @example true
+     */
+    @IsBoolean()
+    isMicUse!: boolean;
+    /**
+     * @example "아지트 이름"
+     */
+    @IsString()
+    azitName!: string;
+    /**
+     * @example "아지트 아이콘 URL"
+     */
+    @IsString()
+    azitIconUrl!: string;
+  }
