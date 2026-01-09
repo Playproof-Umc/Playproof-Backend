08aff60b92d7df4a93456ef66c18b1bc24b9b96c
si-zero <snm306@naver.com>
2026-01-09 23:51:30 +0900

    refactor: 파티 신청 관련 DTO 위치 변경

--- src/modules/party/dtos/party.req.dto.ts
+++ src/modules/party/dtos/party.req.dto.ts
@@ -1,15 +1,1 @@
-import { IsBoolean, IsNumber } from "class-validator";
-import { Type } from "class-transformer";
-
-// 1. 공통 ID 요청 DTO
-export class PartyIdReqDto {
-  @Type(() => Number)
-  @IsNumber()
-  id!: number;
-}
-
-// 2. 매칭 승낙/거절 요청 DTO
-export class PartyApplicationHandleReqDto {
-  @IsBoolean()
-  isAccepted!: boolean;
-}
+export class PartyCreateReqDto {}
