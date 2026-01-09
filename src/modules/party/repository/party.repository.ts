cad094e764269190cc5c1e21b13284f8e0f431b1
rbdus0715 <rbdus0715@gmail.com>
2026-01-09 15:11:00 +0900

    feat: write party repository

--- src/modules/party/repository/party.repository.ts
+++ src/modules/party/repository/party.repository.ts
@@ -1,14 +1,41 @@
+import { prisma } from "../../../common/config/database"; 
 import { singleton } from "tsyringe";
 
 @singleton()
 export class PartyRepository {
-  async createParty(data: any): Promise<void> {}
-
-  async findById(id: number): Promise<any> {
-    return null;
+  async createParty(data: any) {
+    return prisma.partyPost.create({ data });
   }
 
-  async findAll(): Promise<any[]> {
-    return [];
+  async findById(id: number) {
+    return prisma.partyPost.findUnique({ where: { id } });
+  }
+
+  async findByUserId(userId: number) {
+    return prisma.partyPost.findMany({ where: { userId } });
+  }
+
+  async findByGameId(gameId: number) {
+    return prisma.partyPost.findMany({ where: { gameId } });
+  }
+
+  async findByAzitId(azitId: number) {
+    return prisma.partyPost.findMany({ where: { azitId } });
+  }
+
+  async findByPositionId(positionId: number) {
+    return prisma.partyPost.findMany({ where: { positionId } });
+  }
+
+  async findByTierId(tierId: number) {
+    return prisma.partyPost.findMany({ where: { tierId } });
+  }
+
+  async updateParty(id: number, data: any) {
+    return prisma.partyPost.update({ where: { id }, data });
+  }
+
+  async deleteParty(id: number) {
+    return prisma.partyPost.delete({ where: { id } });
   }
 }
