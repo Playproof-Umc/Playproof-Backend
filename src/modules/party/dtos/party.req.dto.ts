f6e00d94161b014897a7c595a3a8cc520b4b986b
si-zero <snm306@naver.com>
2026-01-09 13:36:58 +0900

    feat: 공통 ID 요청 DTO 및 매칭 승낙/거절 요청 DTO 추가

--- src/modules/party/dtos/party.req.dto.ts
+++ src/modules/party/dtos/party.req.dto.ts
@@ -1,1 +1,15 @@
-export class PartyCreateReqDto {}
+import { IsBoolean, IsNumber } from "class-validator";
+import { Type } from "class-transformer";
+
+// 1. 공통 ID 요청 DTO
+export class PartyIdReqDto {
+  @Type(() => Number)
+  @IsNumber()
+  id!: number;
+}
+
+// 2. 매칭 승낙/거절 요청 DTO
+export class PartyApplicationHandleReqDto {
+  @IsBoolean()
+  isAccepted!: boolean;
+}
