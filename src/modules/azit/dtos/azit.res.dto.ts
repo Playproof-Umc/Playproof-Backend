09fff1d693157491968ce3fec8d7a54d33c4c886
ThreeeJ <2171266@hansung.ac.kr>
2026-01-16 12:27:31 +0900

    refactor: AzitResDto에 정적 팩토리 메서드 추가

--- src/modules/azit/dtos/azit.res.dto.ts
+++ src/modules/azit/dtos/azit.res.dto.ts
@@ -1,4 +1,6 @@
 // src/modules/azit/dtos/azit.res.dto.ts
+import { Azit } from '@prisma/client';
+
 export class AzitResDto {
   /**
    * @example 1
@@ -14,6 +16,19 @@ export class AzitResDto {
    * @example "https://s3.ap-northeast-2.amazonaws.com/bucket/example.png"
    */
   azit_icon_url!: string | null;
+
+  /**
+   * 정적 팩토리 메서드
+   * @param azit - Prisma Azit 엔티티
+   * @returns AzitResDto
+   */
+  static from(azit: Pick<Azit, 'id' | 'azitName' | 'imageUrl'>): AzitResDto {
+    return {
+      azit_id: Number(azit.id),
+      azit_name: azit.azitName,
+      azit_icon_url: azit.imageUrl,
+    };
+  }
 }
 
 export class AzitListResDto {
