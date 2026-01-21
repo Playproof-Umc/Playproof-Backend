37db75fa22ae4a094a2d9f9c27cc6a868c2fceee
si-zero <snm306@naver.com>
2026-01-21 14:34:35 +0900

    feat: 커뮤니티 상호작용 DTO 추가

---
+++ src/modules/community/dtos/community-interaction.req.dto.ts
@@ -0,0 +1,26 @@
+import { CommunityTargetType } from "../types/community-type";
+
+/**
+ * 좋아요 토글 요청 DTO
+ */
+export interface CommunityLikeReqDto {
+  target_type: CommunityTargetType;
+  target_id: number;
+}
+
+/**
+ * 댓글 작성 요청 DTO
+ */
+export interface CommunityCommentCreateReqDto {
+  target_type: CommunityTargetType;
+  target_id: number;
+  parent_id?: number;
+  content: string;
+}
+
+/**
+ * 댓글 수정 요청 DTO
+ */
+export interface CommunityCommentUpdateReqDto {
+  content: string;
+}
