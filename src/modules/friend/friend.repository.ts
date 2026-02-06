3c70ac20ecf7670e368fa401c13bffd1d3b9c12d
rbdus0715 <rbdus0715@gmail.com>
2026-02-07 00:28:07 +0900

    feat: 친구 신청 api

--- src/modules/friend/friend.repository.ts
+++ src/modules/friend/friend.repository.ts
@@ -1,4 +1,27 @@
 import { singleton } from 'tsyringe';
+import { FriendRequestReqDto } from './dto/friend.req.dto';
+import { prisma } from '../../common/config/database';
+import { created, Result } from '../../common/types/result.type';
+import { FriendRequestResDto } from './dto/friend.res.dto';
 
 @singleton()
-export class FriendRepository {}
+export class FriendRepository {
+  async friendRequest(
+    userId: number,
+    dto: FriendRequestReqDto,
+  ): Promise<Result<FriendRequestResDto>> {
+    const result = await prisma.friend.create({
+      data: {
+        fromUserId: BigInt(userId),
+        toUserId: BigInt(dto.toUserId),
+        friendStatus: 'PENDING',
+      },
+    });
+    return created({
+      toUserId: dto.toUserId,
+      friendStatus: result.friendStatus,
+      friendAt: result.friendAt,
+      createdAt: result.createdAt,
+    });
+  }
+}
--- src/modules/friend/friend.service.ts
+++ src/modules/friend/friend.service.ts
@@ -1,5 +1,8 @@
 import { inject, injectable } from 'tsyringe';
 import { FriendRepository } from './friend.repository';
+import { FriendRequestReqDto } from './dto/friend.req.dto';
+import { Result } from '../../common/types/result.type';
+import { FriendRequestResDto } from './dto/friend.res.dto';
 
 @injectable()
 export class FriendService {
@@ -7,4 +10,12 @@ export class FriendService {
     @inject(FriendRepository)
     private readonly friendRepository: FriendRepository,
   ) {}
+
+  async friendRequest(
+    userId: number,
+    dto: FriendRequestReqDto,
+  ): Promise<Result<FriendRequestResDto>> {
+    const result = await this.friendRepository.friendRequest(userId, dto);
+    return result;
+  }
 }
