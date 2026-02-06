4a195a5af4fe13b8c3e800a8e4269fac8013c813
rbdus0715 <rbdus0715@gmail.com>
2026-02-07 01:44:05 +0900

    feat: 친구 신청 목록 조회(받은것/내가보낸것) 따로

--- src/modules/friend/friend.controller.ts
+++ src/modules/friend/friend.controller.ts
@@ -7,12 +7,13 @@ import {
   Post,
   Request,
   Body,
+  Get,
 } from 'tsoa/dist';
 import { inject, injectable } from 'tsyringe';
 import { FriendService } from './friend.service';
 import { FriendRequestReqDto } from './dto/friend.req.dto';
 import { Result } from '../../common/types/result.type';
-import { FriendRequestResDto } from './dto/friend.res.dto';
+import { FriendItemResDto, FriendListResDto, FriendRequestResDto } from './dto/friend.res.dto';
 
 @Route('friends')
 @Tags('Friend')
@@ -33,6 +34,28 @@ export class FriendController extends Controller {
   ): Promise<Result<FriendRequestResDto>> {
     const userId = req.user.id;
     const result = await this.friendService.friendRequest(userId, dto);
+    this.setStatus(result.statusCode);
+    return result;
+  }
+
+  // 내가 신청했던 친구들의 목록을 조회(Pending, Accepted 모두 포함)
+  @SuccessResponse('200', 'OK')
+  @Security('jwt')
+  @Get('/list')
+  async getSentFriendList(@Request() req: any): Promise<Result<FriendListResDto>> {
+    const userId = req.user.id;
+    const result = await this.friendService.getSentFriendList(userId);
+    this.setStatus(result.statusCode);
+    return result;
+  }
+
+  // 나에게 온 친구 신청자 목록을 조회
+  @SuccessResponse('200', 'OK')
+  @Security('jwt')
+  @Get('/request/list')
+  async getReceivedFriendList(@Request() req: any): Promise<Result<FriendListResDto>> {
+    const userId = req.user.id;
+    const result = await this.friendService.getReceivedFriendList(userId);
     this.setStatus(result.statusCode);
     return result;
   }
--- src/modules/friend/friend.service.ts
+++ src/modules/friend/friend.service.ts
@@ -1,8 +1,9 @@
 import { inject, injectable } from 'tsyringe';
 import { FriendRepository } from './friend.repository';
 import { FriendRequestReqDto } from './dto/friend.req.dto';
-import { Result } from '../../common/types/result.type';
-import { FriendRequestResDto } from './dto/friend.res.dto';
+import { created, ok, Result } from '../../common/types/result.type';
+import { FriendListResDto, FriendRequestResDto } from './dto/friend.res.dto';
+import { validateUserExists } from './utils/friend.validator';
 
 @injectable()
 export class FriendService {
@@ -15,7 +16,41 @@ export class FriendService {
     userId: number,
     dto: FriendRequestReqDto,
   ): Promise<Result<FriendRequestResDto>> {
+    // 있는 사람인지 검증 => 없으면 404 반환
+    // 이미 친구 추가 눌렀는지 검증 => 이미 추가했으면 400 반환
     const result = await this.friendRepository.friendRequest(userId, dto);
-    return result;
+    return created({
+      toUserId: dto.toUserId,
+      friendStatus: result.friendStatus,
+      friendAt: result.friendAt,
+      createdAt: result.createdAt,
+    });
+  }
+
+  async getSentFriendList(userId: number): Promise<Result<FriendListResDto>> {
+    // 있는 사람인지 검증 => 없으면 404 반환
+    const userCheckResult = validateUserExists(userId);
+    if (userCheckResult.error) {
+      return userCheckResult;
+    }
+
+    const result = await this.friendRepository.getFriendList(userId, true);
+    return ok({
+      friends: result,
+    });
+  }
+
+  async getReceivedFriendList(
+    userId: number,
+  ): Promise<Result<FriendListResDto>> {
+    const userCheckResult = validateUserExists(userId);
+    if (userCheckResult.error) {
+      return userCheckResult;
+    }
+
+    const result = await this.friendRepository.getFriendList(userId, false);
+    return ok({
+      friends: result,
+    });
   }
 }
