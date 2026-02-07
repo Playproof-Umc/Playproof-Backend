1f50634a9035c716302e185e476a81f04c8b2f7f
rbdus0715 <rbdus0715@gmail.com>
2026-02-07 23:49:17 +0900

    fix: rest 원칙에 맞춰 엔드포인트 수정

--- src/modules/friend/controllers/friend.controller.ts
+++ src/modules/friend/controllers/friend.controller.ts
@@ -36,7 +36,7 @@ export class FriendController extends Controller {
   // 내 친구들 목록 조회
   @SuccessResponse('200', 'OK')
   @Security('jwt')
-  @Get('/list')
+  @Get('')
   async getFriendList(
     @Request() req: any,
   ): Promise<Result<FriendItemResDto[]>> {
@@ -48,7 +48,7 @@ export class FriendController extends Controller {
 
   @SuccessResponse('200', 'OK')
   @Security('jwt')
-  @Post('/request')
+  @Post('/requests')
   async friendRequest(
     @Request() req: any,
     @Body() dto: FriendRequestReqDto,
@@ -62,7 +62,7 @@ export class FriendController extends Controller {
   // 내가 신청했던 친구들의 목록을 조회(Pending, Accepted 모두 포함)
   @SuccessResponse('200', 'OK')
   @Security('jwt')
-  @Get('/sent/list')
+  @Get('/requests/sent')
   async getSentFriendList(
     @Request() req: any,
   ): Promise<Result<FriendListResDto>> {
@@ -75,7 +75,7 @@ export class FriendController extends Controller {
   // 나에게 온 친구 신청자 목록을 조회
   @SuccessResponse('200', 'OK')
   @Security('jwt')
-  @Get('/received/list')
+  @Get('/requests/received')
   async getReceivedFriendList(
     @Request() req: any,
   ): Promise<Result<FriendListResDto>> {
@@ -88,7 +88,7 @@ export class FriendController extends Controller {
   // 친구 수락하기
   @SuccessResponse('200', 'OK')
   @Security('jwt')
-  @Patch('/request/{requestId}/accept')
+  @Patch('/requests/{requestId}')
   async acceptFriendRequest(
     @Request() req: any,
     @Path() requestId: number,
