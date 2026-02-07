1265dcc76be02c2be05305e9f3c89ada89196691
rbdus0715 <rbdus0715@gmail.com>
2026-02-07 23:39:19 +0900

    chore: 폴더 분리

--- src/modules/friend/controllers/friend.controller.ts
+++ src/modules/friend/controllers/friend.controller.ts
@@ -14,15 +14,15 @@ import {
   Delete,
 } from 'tsoa/dist';
 import { inject, injectable } from 'tsyringe';
-import { FriendService } from './friend.service';
-import { FriendRequestReqDto } from './dto/friend.req.dto';
-import { Result } from '../../common/types/result.type';
+import { FriendService } from '../services/friend.service';
+import { FriendRequestReqDto } from '../dto/friend.req.dto';
+import { Result } from '../../../common/types/result.type';
 import {
   FriendAcceptResDto,
   FriendItemResDto,
   FriendListResDto,
   FriendRequestResDto,
-} from './dto/friend.res.dto';
+} from '../dto/friend.res.dto';
 
 @Route('friends')
 @Tags('Friend')
@@ -125,7 +125,10 @@ export class FriendController extends Controller {
     @Query() nickname: string,
   ): Promise<Result<FriendItemResDto[]>> {
     const userId = req.user.id;
-    const result = await this.friendService.searchFriendByNickname(userId, nickname);
+    const result = await this.friendService.searchFriendByNickname(
+      userId,
+      nickname,
+    );
     this.setStatus(result.statusCode);
     return result;
   }
