7f7890c9d8fedf181f05eadeadf4afb5488c8ddc
miinsoo <ms1226f@gmail.com>
2026-02-10 12:19:55 +0900

    fix: 마이그레이션 오류 해결

--- prisma/migrations/20260209190857_fix_schema_drift/migration.sql
+++ prisma/migrations/20260209190857_fix_schema_drift/migration.sql
@@ -8,9 +8,29 @@
   - A unique constraint covering the columns `[positive_category_name]` on the table `positive_categories` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[name]` on the table `shop_items` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[name]` on the table `terms` will be added. If there are existing duplicate values, this will fail.
-  - A unique constraint covering the columns `[tier_name]` on the table `tiers` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `schedule_id` to the `feedbacks` table without a default value. This is not possible if the table is not empty.
 
 */
+-- AlterTable
+ALTER TABLE `feedbacks` ADD COLUMN `schedule_id` BIGINT NOT NULL,
+    ADD COLUMN `ts_score_change` INTEGER NOT NULL DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE `user_game_info` ADD COLUMN `account_id` VARCHAR(191) NULL;
+
+-- AlterTable
+ALTER TABLE `users` ADD COLUMN `statusMessage` VARCHAR(191) NULL;
+
+-- CreateTable
+CREATE TABLE `chat_room_member` (
+    `member_id` BIGINT NOT NULL,
+    `room_id` BIGINT NOT NULL,
+    `role` ENUM('creator', 'participant') NOT NULL DEFAULT 'participant',
+    `participation_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`member_id`, `room_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- CreateIndex
 CREATE UNIQUE INDEX `categories_category_name_key` ON `categories`(`category_name`);
 
@@ -32,5 +52,11 @@ CREATE UNIQUE INDEX `shop_items_name_key` ON `shop_items`(`name`);
 -- CreateIndex
 CREATE UNIQUE INDEX `terms_name_key` ON `terms`(`name`);
 
--- CreateIndex
-CREATE UNIQUE INDEX `tiers_tier_name_key` ON `tiers`(`tier_name`);
+-- AddForeignKey
+ALTER TABLE `chat_room_member` ADD CONSTRAINT `chat_room_member_member_id_fkey` FOREIGN KEY (`member_id`) REFERENCES `azit_user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `chat_room_member` ADD CONSTRAINT `chat_room_member_room_id_fkey` FOREIGN KEY (`room_id`) REFERENCES `chat_rooms`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `feedbacks` ADD CONSTRAINT `feedbacks_schedule_id_fkey` FOREIGN KEY (`schedule_id`) REFERENCES `azit_schedule`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
